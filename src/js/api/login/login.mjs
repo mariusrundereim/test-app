d8f669ae328888e33fdf4dfbd69b71c6686c5e23
mariusrundereim <marius.rundereim@gmail.com>
2023-10-25 13:50:13 +0200

    upload

--- src/js/api/login/login.mjs
+++ src/js/api/login/login.mjs
@@ -1,6 +1,19 @@
-console.log(baseUrl);
+import { baseUrl } from "./baseurl.mjs";
+
 export async function login(username, password) {
-  const response = await fetch(`${baseUrl}, options`);
+  const options = {
+    method: "POST",
+    body: JSON.stringify({
+      username: username,
+      password: password,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
+  const response = await fetch(`${baseUrl}`, options);
   const result = await response.json();
   console.log(result);
+  return result;
 }
+login();
